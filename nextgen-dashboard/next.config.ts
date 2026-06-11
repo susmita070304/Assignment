5161b5da2bd4796fb5774ea6e725bf96a2197782
susmita070304 <skar99198@gmail.com>
2026-06-11 21:34:49 +0530

    implimented Framer Motion

--- nextgen-dashboard/next.config.ts
+++ nextgen-dashboard/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  allowedDevOrigins: ["172.30.64.1", "localhost:3000"], 
+  
 };
 
 export default nextConfig;
